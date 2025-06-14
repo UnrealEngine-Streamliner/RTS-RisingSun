4897477ecb6aa48ee57c53377f9c75ebe08ef197
tiger <thisisstreamliner@gmail.com>
2025-06-14 18:51:48 +0300

    feat: camera zoom

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -1,15 +1,73 @@
 using System;
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour
 {
     [SerializeField] private Transform cameraTarget;
+    [SerializeField] private CinemachineCamera cinemachineCamera;
     [SerializeField] private float keyboardPanSpeed = 5;
+    [SerializeField] private float zoomSpeed = 1;
+    [SerializeField] private float minZoomDistance = 7.5f;
 
-    void Update()
+    private CinemachineFollow cinemachineFollow;
+    private float zoomStartTime;
+    private Vector3 startingFollowOffset;
+
+    private void Awake()
+    {
+        if (!cinemachineCamera.TryGetComponent<CinemachineFollow>(out cinemachineFollow))
+        {
+            Debug.LogError("Cinemachine Camera did not have CinemachineFollow. Zoom functionality will not work");
+        }
+
+        startingFollowOffset = cinemachineFollow.FollowOffset;
+    }
+
+    private void Update()
     {
         HandleKeyboardInputMovement();
+        HandleZooming();
+    }
+
+    private void HandleZooming()
+    {
+        if (ShouldSetZoomStartTime())
+        {
+            zoomStartTime = Time.time;
+        }
+
+        Vector3 targetFollowOffset;
+
+        float zoomTime = Mathf.Clamp01((Time.time - zoomStartTime) * zoomSpeed);
+
+        if (Keyboard.current.zKey.isPressed)
+        {
+            targetFollowOffset = new Vector3(
+                cinemachineFollow.FollowOffset.x,
+                minZoomDistance,
+                cinemachineFollow.FollowOffset.z
+            );
+        }
+        else
+        {
+            targetFollowOffset = new Vector3(
+                cinemachineFollow.FollowOffset.x,
+                startingFollowOffset.y,
+                cinemachineFollow.FollowOffset.z
+            );
+        }
+        cinemachineFollow.FollowOffset = Vector3.Slerp(
+            cinemachineFollow.FollowOffset,
+            targetFollowOffset,
+            zoomTime
+        );
+    }
+
+    private bool ShouldSetZoomStartTime()
+    {
+        return Keyboard.current.zKey.wasPressedThisFrame || Keyboard.current.zKey.wasReleasedThisFrame;
     }
 
     private void HandleKeyboardInputMovement()
