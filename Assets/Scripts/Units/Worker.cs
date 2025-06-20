29f4c4b055631afba616efbf780db49f93878017
tiger <thisisstreamliner@gmail.com>
2025-06-20 19:28:03 +0300

    feat: setup nav_mesh_agent

---
+++ Assets/Scripts/Units/Worker.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Worker : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+}
